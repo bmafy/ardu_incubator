9823027d4b2005e643d5882fa35bd1f6dd7238fd
bmafy <bmafy@hotmail.it>
2016-02-08 15:04:09 +0100

    Update paho.mqtt.plugin.js

--- paho.mqtt.plugin.js
+++ paho.mqtt.plugin.js
@@ -10,7 +10,7 @@
 		"display_name": "Paho MQTT",
         "description" : "Receive data from an MQTT server.",
 		"external_scripts" : [
-			"https://rawgit.com/benjaminchodroff/freeboard-mqtt/paho-mqtt-default/mqttws31.js"
+			"http://git.eclipse.org/c/paho/org.eclipse.paho.mqtt.javascript.git/plain/src/mqttws31.js"
 		],
 		"settings"    : [
 			{
